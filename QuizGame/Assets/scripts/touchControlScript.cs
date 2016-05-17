e7108425c6ce38820ce8c5b66f1f5c438d82641a
SGAoo7 <Jens@iMac-van-Jens.local>
2016-05-17 21:20:52 +0200

    1ste touch test

--- QuizGame/Assets/scripts/touchControlScript.cs
+++ QuizGame/Assets/scripts/touchControlScript.cs
@@ -4,18 +4,26 @@ using System.Collections;
 public class touchControlScript : MonoBehaviour {
 
     public GameObject movingObject;
+	private float movingObjectX;
+
     public bool moving;
 
-    void Update() {
-        if (Input.touchCount > 1) {
-            Touch touch = Input.GetTouch(1);
+	void Awake() {
+		movingObjectX = movingObject.transform.position.x;
+	}
 
-            if (touch.phase == TouchPhase.Began) {
-                moving = true;
-            }
-            if (touch.phase == TouchPhase.Ended) {
-                moving = false;
-            }
-        }
+    void Update() {
+		foreach (Touch touch in Input.touches) {
+			if(touch.phase == TouchPhase.Began) {
+				moving = true;
+			}
+			if(touch.phase == TouchPhase.Ended) {
+				moving = false;
+			}
+			if (moving == true) {
+				movingObjectX += touch.deltaPosition.x * Time.deltaTime;
+				movingObject.transform.position = new Vector2 (movingObjectX, 0f);
+			}
+		}
     }
 }
